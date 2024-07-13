f43c15b147a6c8870c998fd2a3e4450cc8868203
Nicholas Chai <nicholas.chai@outlook.com>
2024-07-13 18:38:09 +0800

    added todo list item inputs

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Button } from "@mui/material";
+import { Typography, Box, Button, TextField } from "@mui/material";
 import {useState} from "react"
 
 type ToDo = {
@@ -11,25 +11,38 @@ type ToDos = {
 }
 
 const ToDoList = () => {
-	const [todos, setTodos] = useState<ToDos>({items: []})
+	const [todos, setTodos] = useState<ToDos>({items: []});
+	const [inputText, setInputText] = useState<string>("");
 
 	const handleAddToDo = () => {
+		if (inputText === "") {
+			return
+		}
 		setTodos(
 			prevTodos => ({
 				items: [
 					...prevTodos.items,
 					{
 						id: prevTodos.items.length + 1,
-						content: "new item"
+						content: inputText
 					}
 				]
 			})
 		);
+		setInputText("");
 	}
+
+	const handleInputChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInputText(event.target.value);
+	};
 
 	return(
 		<>
-			<Box>
+			<Box
+				sx={{
+					textAlign: "left"
+				}}
+			>
 				{todos.items.map((item) => {
 					return (
 						<Typography
@@ -42,13 +55,31 @@ const ToDoList = () => {
 						</Typography>
 					);
 				})}
-				<Button
-					variant="contained"
-					onClick={handleAddToDo}
-					sx={{
-						mt: 2
-					}}
-				>Add</Button>
+				<Box>
+					<TextField 
+						sx={{
+							width: "180px",
+							mr: 1,
+							mt: 2,
+							'& .MuiInputBase-root': {
+								height: 37,  // Set the height you want
+							},
+							'& .MuiOutlinedInput-input': {
+								padding: '10px 14px',  // Adjust padding to fit the new height
+							}
+						}}
+						onChange={handleInputChanges}
+						value={inputText}
+					/>
+					<Button
+						variant="contained"
+						onClick={handleAddToDo}
+						sx={{
+							mt: 2
+						}}
+					>Add</Button>
+				</Box>
+
 			</Box>
 		</>
 	);
